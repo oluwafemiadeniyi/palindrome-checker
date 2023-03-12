7586ade2c983b2209269b537b2db9473d953bc27
oluwafemiadeniyi <adeniyioluwafemi595@gmail.com>
2023-03-12 10:15:33 +0100

    create another version of the str list in the same order as the input string

--- paleondrome checker.js
+++ paleondrome checker.js
@@ -4,4 +4,7 @@ function palindrome(str) {
     str=str.split('')
     //create a version of the str list in reverse order
     let str1l=str.reduce(function(revstr,a){if(!/[^a-z0-9]/i.test(a)) revstr.unshift(a); return revstr},[])
-    console.log(str1l)}
+    console.log(str1l)
+    //create another version of the str list in the same order as the input string
+    let str2l=str.reduce(function(revstr,a){if(!/[^a-z0-9]/i.test(a)) revstr.push(a); return revstr},[])
+    console.log(str2l)}
