f374de4e13c72bef46d3b2d194506a77e133d3b5
oluwafemiadeniyi <adeniyioluwafemi595@gmail.com>
2023-03-12 10:18:11 +0100

    compare each element of the reversed list and the normal ordered list and return the truth value if they are all equal or not

--- paleondrome checker.js
+++ paleondrome checker.js
@@ -7,4 +7,6 @@ function palindrome(str) {
     console.log(str1l)
     //create another version of the str list in the same order as the input string
     let str2l=str.reduce(function(revstr,a){if(!/[^a-z0-9]/i.test(a)) revstr.push(a); return revstr},[])
-    console.log(str2l)}
+    console.log(str2l)
+    //compare each element of the reversed list and the normal ordered list and return the truth value if they are all equal or not
+    return str1l.every((a,i)=>str1l[i]==str2l[i]}
